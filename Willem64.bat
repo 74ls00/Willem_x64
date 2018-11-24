7518695f3e647965733554fd673aa830871ee420
74ls00 <userled@yandex.ru>
2018-11-24 18:50:35 +0300

    .

--- Willem64.bat
+++ Willem64.bat
@@ -1,48 +1,71 @@
+echo off
+chcp 65001
 
-
-Set xOS=x64& If "%PROCESSOR_ARCHITECTURE%"=="x86" (
- If Not Defined PROCESSOR_ARCHITEW6432 Set xOS=x86
+Set "xOS=x64" & If "%PROCESSOR_ARCHITECTURE%"=="x86" (
+ If Not Defined PROCESSOR_ARCHITEW6432 Set "xOS=x86"
 )
-
-
-
-cls
-Echo OS - %xOS%
 
 set wpath=%~d0%~p0bin
 set dpath=%~d0%~p0bin
 set soft98=EpromM51_98D12C3.exe
 set drvinst=TVicPortInstall41.exe
 
+set GenteePaths="HKLM\SOFTWARE\Wow6432Node\Gentee\Paths"
+set "DrvPathsName=TVicPortDemo"
+set "DrvPaths="
+
+
+
 cls
 
-rem set xOS="x86"
 
-@echo %xOS%
+rem set "xOS=x86"
+rem set "xOS=x64"
 
-if %xOS% NEQ "x64" goto run86
-if %xOS% EQU "x64" goto run64
+@echo xOS=%xOS%
+
+if %xOS% EQU x64 goto :run64
+if %xOS% NEQ x64 goto :run86
+
 @echo end
-goto endbat
+goto :endbat
 
 :run86
+pause
 echo mode 86
-
 start /d  "%wpath%" %soft98%
-
-
-goto endbat
+goto :endbat
 
 :run64
 
-echo 64
+echo mode 64
 
-if not exist "T:\Temp\4" (start "TVicPort Install ..." /wait  /d "%dpath%" %drvinst%) else (start /d  "%wpath%" %soft98%)
+rem < чтение пути из реестра
+for /f "tokens=2*" %%A in ('REG QUERY %GenteePaths% /v %DrvPathsName%') DO (
+  for %%F in (%%B) do (
+    set DrvPaths=%%F
+    goto :break1
+  )
+)
+:break1
+rem end чтение пути из реестра >
 
-rem EQL %xOS%="x64"
+rem проверка наличия файлов
+if exist %DrvPaths%\cpd64.exe (Call echo %DrvPaths%\cpd64.exe               OK) else (Echo %DrvPaths%\cpd64.exe               не найден)
+if exist %DrvPaths%\Docs\LICENSE.TXT (echo %DrvPaths%\Docs\LICENSE.TXT        OK) else (Echo %DrvPaths%\Docs\LICENSE.TXT        не найден)
+if exist %DrvPaths%\Examples (echo %DrvPaths%\Examples                найдено) else (Echo %DrvPaths%\Examples                не установлено)
+if exist %DrvPaths%\uninstall.ini (echo %DrvPaths%\uninstall.ini           OK) else (Echo %DrvPaths%\uninstall.ini           не найден)
+if exist %DrvPaths%\uninstall.exe (echo %DrvPaths%\uninstall.exe           OK) else (Echo %DrvPaths%\uninstall.exe           не найден)
+
+if exist %SystemRoot%\system\TVicPort.dll (echo %SystemRoot%\system\TVicPort.dll              OK) else (Echo %SystemRoot%\system\TVicPort.dll              не найден)
+if exist %SystemRoot%\System32\drivers\TVicPort64.sys (echo %SystemRoot%\System32\drivers\TVicPort64.sys  OK) else (Echo %SystemRoot%\System32\drivers\TVicPort64.sys  не найден)
+if exist %SystemRoot%\SysWOW64\drivers\TVicPort64.sys (echo %SystemRoot%\SysWOW64\drivers\TVicPort64.sys  OK) else (Echo %SystemRoot%\SysWOW64\drivers\TVicPort64.sys  не найден)
+
+if not exist "%DrvPaths%\cpd64.exe" (start "TVicPort Install ..." /wait  /d "%dpath%" %drvinst%) else (start /d  "%wpath%" %soft98%)
 
 
-pause
+
+
 
 
 
@@ -52,6 +75,6 @@ goto endbat
 
 :endbat
 
-
-
+echo exit
 Pause
+exit
---
+++ bin/readme.txt
@@ -0,0 +1,6 @@
+add files
+
+EpromM51_98D12C3.exe
+io.dll
+io.ini
+TVicPortInstall41.exe
